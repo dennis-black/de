4d0b6ea6a6a55e04e89ea40d6506ebcac36323f8
dengyi <cbb111227@nptu.edu.tw>
2025-03-26 22:23:16 +0800

    update

---
+++ black-cat-studio/toolBoxContent/magicLittleTrash/dokuwikiTransformer/script.js
@@ -0,0 +1,51 @@
+function addNewResult() {
+    let count = 1;
+    while (true) {
+        if (document.getElementsByClassName("result")[count++] == null) {
+            // alert(count);
+            break;
+        }
+    }
+
+    const newResult = document.createElement("div");
+    newResult.innerHTML = "<div>執行結果" + count.toString() + "</div><textarea style=\"width: 100%;\" id=\"result" + count.toString() + "\" cols=\"30\" rows=\"5\"></textarea>";
+    newResult.className = "result";
+
+    const results = document.getElementsByClassName("result");
+    if (results.length > 0) {
+        const lastResult = results[results.length - 1];
+        lastResult.appendChild(newResult);
+    } else {
+        console.error("No elements with class 'result' found.");
+    }
+    return null;
+}
+
+function exportDokuwiki() {
+    const prompt = document.getElementById("prompt");
+    if (!prompt) {
+        console.error("Element with id 'prompt' not found.");
+        return;
+    }
+
+    let result = "";
+    const results = document.getElementsByClassName("result");
+    for (let i = 0; i < results.length; i++) {
+        const textarea = results[i].querySelector("textarea");
+        if (textarea) {
+            result += prompt.value + ` \'\'./a.out↵\'\'\\\\` + '\n' + textarea.value.replace(/ /g, '▴').replace(/\n/g, '↵\\\\\n');
+            result += '↵\\\\\n';
+        }
+    }
+    result += prompt.value;
+
+    const exportArea = document.getElementById("transfered");
+    if (exportArea) {
+        exportArea.value = result;
+        document.getElementById("transferedBlock").style.display = "block";
+    } else {
+        console.error("Element with id 'transfered' not found.");
+    }
+
+    return null;
+}
