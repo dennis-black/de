9ee43ea8372c9dcb518a1028a216426bf774b948
dennis-black <d3nni581ack@gmail.com>
2025-04-19 02:06:28 +0800

    feat: integrate SweetAlert2 for new site prompt across multiple files

--- black-cat-studio/toolBoxContent/magicLittleTrash/creditAnalyse/calculate.js
+++ black-cat-studio/toolBoxContent/magicLittleTrash/creditAnalyse/calculate.js
@@ -1,4 +1,21 @@
 
+document.addEventListener("DOMContentLoaded", () => {
+    Swal.fire({
+        title: "黑貓工作室已經有新網站了，要去看看嗎？",
+        text: "新網站有固定的域名了！dennis-black.dev",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "好啊",
+        cancelButtonText: "不要"
+    }).then((result) => {
+        if (result.isConfirmed) {
+            location.href = "https://dennis-black.dev";
+        }
+    });
+});
+
 var creditDetail = []; //將使用者輸入的學分詳細內容推入
 var mistakeDetail = ""; //將使用者輸入的不在《111年資訊工程學系-學生手冊》課程編號推入
 var creditSum = 0; //總學分
--- black-cat-studio/toolBoxContent/magicLittleTrash/dokuwikiTransformer/script.js
+++ black-cat-studio/toolBoxContent/magicLittleTrash/dokuwikiTransformer/script.js
@@ -1,3 +1,21 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+    Swal.fire({
+        title: "黑貓工作室已經有新網站了，要去看看嗎？",
+        text: "新網站有固定的域名了！dennis-black.dev",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "好啊",
+        cancelButtonText: "不要"
+    }).then((result) => {
+        if (result.isConfirmed) {
+            location.href = "https://dennis-black.dev";
+        }
+    });
+});
+
 function addNewResult() {
     let count = 1;
     while (true) {
