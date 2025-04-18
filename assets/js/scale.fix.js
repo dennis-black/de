6e50b151c5abbbf06092008dba4b51ecccfbbd68
dennis-black <d3nni581ack@gmail.com>
2025-04-19 02:01:36 +0800

    refactor: move new site prompt to scale.fix.js and remove old script

--- assets/js/scale.fix.js
+++ assets/js/scale.fix.js
@@ -25,3 +25,20 @@
         document.addEventListener("touchend", gestureEnd, false);
     }
 })(document);
+
+document.addEventListener("DOMContentLoaded", () => {
+    Swal.fire({
+        title: "黑貓工作室已經有新網站了，要去看看嗎？",
+        text: "新網站有固定的域名了！dennis-black.dev",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "好啊",
+        cancelButtonText: "不要"
+    }).then((result) => {
+        if (result.isConfirmed) {
+            location.href = "https://dennis-black.dev";
+        }
+    });
+});
